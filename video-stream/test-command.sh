a3b3ec809ee891c49e9bd9a2f15df7d4383923c5
Julien JEHL <julien.jehl@roboticia.com>
2025-04-06 11:44:59 +0200

    add config

--- video-stream/test-command.sh
+++ video-stream/test-command.sh
@@ -6,3 +6,19 @@ libcamera-vid --list-cameras
 
 # Record a 1s video
 libcamera-vid -t 10000 -o test.h264
+
+# record an image
+libcamera-still --awb off  -o image.jpg
+libcamera-jpeg -o test.jpg
+
+# record in IR mode
+libcamera-still --awbgains 0,0 --saturation 0 -t 2000 -o image.jpg
+libcamera-vid --awbgains 0,0 --saturation 0 -t 5000 --output ir_video.h264
+
+# configure rpi zero
+sudo nano /boot/firmware/config.txt
+#Find the line: camera_auto_detect=1, update it to:
+camera_auto_detect=0
+#Find the line: [all], add the following item under it:
+dtoverlay=ov5647
+#Save and reboot.
