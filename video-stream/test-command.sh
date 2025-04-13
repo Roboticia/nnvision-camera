d05dbdcadf37c7eff69839fc082fb0366b7f3641
Julien JEHL <julien.jehl@roboticia.com>
2025-04-13 10:11:59 +0200

    add config

--- video-stream/test-command.sh
+++ video-stream/test-command.sh
@@ -8,17 +8,38 @@ libcamera-vid --list-cameras
 libcamera-vid -t 10000 -o test.h264
 
 # record an image
-libcamera-still --awb off  -o image.jpg
+libcamera-still  -o image.jpg
 libcamera-jpeg -o test.jpg
+libcamera-still --mode 2592:1944:10:P -o image.jpg
+libcamera-still --mode 640:480:10:P -o image.jpg
+
 
 # record in IR mode
 libcamera-still --awbgains 0,0 --saturation 0 -t 2000 -o image.jpg
 libcamera-vid --awbgains 0,0 --saturation 0 -t 5000 --output ir_video.h264
 
-# configure rpi zero
+# ------------------------------  configure rpi on the right sensor
 sudo nano /boot/firmware/config.txt
 #Find the line: camera_auto_detect=1, update it to:
 camera_auto_detect=0
 #Find the line: [all], add the following item under it:
 dtoverlay=ov5647
 #Save and reboot.
+
+# Change resolution
+libcamera-vid --mode 640:480:10:P -t 10000 -o video.h264
+libcamera-vid --mode 2592:1944:10:P -t 10000 -o video.h264
+
+# -------------------------------------------  memory CMA
+grep -i cma /proc/meminfo
+dmesg | grep -i cma
+# to change CMA size add to /boot/firmware/config.txt
+dtoverlay=cma,cma-96
+
+# ---------------------------------------------- memory GPU
+vcgencmd get_mem arm && vcgencmd get_mem gpu
+# to change GPU memory add to /boot/firmware/config.txt
+gpu_mem=128
+
+# some documentation from arducam
+https://docs.arducam.com/Raspberry-Pi-Camera/Native-camera/5MP-OV5647/#hardware-connection
